b96eb2a6757cf7472ff94b5fb36f3429e70d7e8d
YBhatta2058 <ybhatta70@gmail.com>
2023-06-02 13:13:40 +0530

    Databases configured

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -78,10 +78,10 @@ CORS_ORIGIN_WHITELIST = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 

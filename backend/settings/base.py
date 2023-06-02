b0f0d0f9ee87b1d041a88e2374776abd317a4bd9
YBhatta2058 <ybhatta70@gmail.com>
2023-06-02 14:20:39 +0530

    POSTGRESQL

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -79,8 +79,10 @@ CORS_ORIGIN_WHITELIST = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'apiblogs',
+        'USER':'Yogesh',
+        'PASSWORD':'Barcelona@58'
     }
 }
 

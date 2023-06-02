180d0a27c16267d25613091ebd522fa9c400de38
YBhatta2058 <ybhatta70@gmail.com>
2023-06-02 13:41:53 +0530

    Final Changes

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -3,9 +3,10 @@ from .base import *
 
 from dotenv import load_dotenv
 
+dotenv_path = os.path.join(BASE_DIR,'.env')
 
-load_dotenv()
+load_dotenv(dotenv_path = dotenv_path)
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG')
+DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh'] 
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1'] 

8fcaccab52619b0d93ac11d39b7b6e120500ebe9
YBhatta2058 <ybhatta70@gmail.com>
2023-06-03 16:17:17 +0530

    TRUE

--- backend/urls.py
+++ backend/urls.py
@@ -25,8 +25,5 @@ urlpatterns = [
     path('',include('holdApi.urls'))    
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 
